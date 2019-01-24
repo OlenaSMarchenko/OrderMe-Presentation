b1bdea5251333bce8e2962bbd0b15ca03de0eb8b
Olena <olena@Olenas-iMac.local>
2019-01-24 15:49:58 -0800

    Add TapOnProtocol

---
+++ OrderMEUITests/Support/TapBarProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  TapBarProtocol.swift
+//  OrderMEUITests
+//
+//  Created by Olena on 1/24/19.
+//  Copyright © 2019 Boris Gurtovoy. All rights reserved.
+//
+
+import Foundation
+
+protocol TapBarProtocol {
+    func goToMyReservations ()
+    }
+extension TapBarProtocol {
+    func goToMyReservations () {
+        let myReservationsBtn = BaseScreen.app.buttons["My Reservations"]
+        myReservationsBtn.tap()
+    }
+}
